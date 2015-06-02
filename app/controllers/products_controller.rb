6722b7cd8aa9c0b5b898931188722092adf48531
Sam O'Connell <sjo773@gmail.com>
2015-06-01 19:30:49 -0500

    initial commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,18 @@
+class ProductsController < ApplicationController
+
+  def index
+    @products = Product.all
+  end
+  
+  def show
+    @product = Product.find_by(id: params[:id])
+  end
+  
+  def new
+  end
+  
+  def create
+    Product.create(name: params[:name], price: params[:price], image: params[:image], description: params[:description])
+  end
+
+end
