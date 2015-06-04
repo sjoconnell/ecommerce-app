8053b63066496b17977cddc89c73e0895d07c18c
Sam O'Connell <sjo773@gmail.com>
2015-06-03 21:04:15 -0500

    added tax

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ActiveRecord::Base
+
+def sale_message
+  discount_message = ""
+  discount_message += "Discount Item!" if price.to_i < 2
+  discount_message += "Sale Item!" if price.to_i > 2
+  discount_message
+end
+
+def taxify
+  price.to_f * 0.09
+end
+
+def total
+  price.to_f * 1.09
+end
+
+  
 end
