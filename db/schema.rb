78056c4d9e4faaf0a35eedad4f068012936d9320
Sam O'Connell <sjo773@gmail.com>
2015-06-04 19:59:58 -0500

    added available column to db

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529004519) do
+ActiveRecord::Schema.define(version: 20150605005547) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
-    t.string   "price"
+    t.decimal  "price",       precision: 8, scale: 2
     t.string   "image"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.text     "description"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.boolean  "available"
   end
 
 end
