13fe54915d67ce84a261bc374b5e818d2a7fb232
Sam O'Connell <sjo773@gmail.com>
2015-06-17 20:11:36 -0500

    added shopping cart count

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,15 +4,5 @@ class Order < ActiveRecord::Base
   has_many :users, through: :carted_products
   has_many :products, through: :carted_products
 
-  def calculate_subtotal(price)
-    quantity * price
-  end
 
-  def calculate_tax(price)
-    quantity * price * 0.09
-  end
-
-  def calculate_total(price)
-    quantity * price * 1.09
-  end
 end
